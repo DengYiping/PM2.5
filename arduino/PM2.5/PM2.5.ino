c169b1f72c5a4c1bb9547de2a80b998390026c4a
DengYiping <scottdeng@live.cn>
2016-05-09 20:48:57 +0800

    Remove LCD support, modify parameter to suit the need of Arduino Nano 3.0 and Bluetooth model.

--- arduino/PM2.5/PM2.5.ino
+++ arduino/PM2.5/PM2.5.ino
@@ -3,14 +3,17 @@
 // 0.95 is reference voltage
 // 25.8 is temperature of reference voltage
 //包含头文件
-#include <LiquidCrystal.h>
-//定义使用的I/O口
-LiquidCrystal lcd(12,11,5,4,3,2);
-
+#include<DHT.h>
 #define measurePin A5 // 连接模拟口0
  
 #define ledPower  8 // 连接数字口2
 
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#define REFVOLT 760
 int count = 0;
 float averageP = 0; // reading of DN7C3
 float valueP = 0;
@@ -22,15 +25,11 @@ unsigned long A = 0;
 unsigned long B = 0;
  
 void setup() {
-  
-    //设置lcd显示的行数与列数
-    lcd.begin(16,2);
-    lcd.print("PM2.5 Sensor");
     pinMode(measurePin, INPUT); // DN7C3 Vo Pin
     pinMode(ledPower, OUTPUT);
     digitalWrite(ledPower, HIGH);
  
-    Serial.begin(115200);
+    Serial.begin(9600);
     Serial.println("Sharp DN7C3 PM2.5 Sensor");
     
     for(int i = 0; i < 1000; i++) {
@@ -48,7 +47,6 @@ void setup() {
 void loop() {
     if ((micros() - timer) >= 10000) {  // per 10ms
         timer = micros();
- 
         digitalWrite(ledPower, LOW);    // turn on led
         A = micros();            // measure pulse begin time
         delayMicroseconds(227);  // Arduino ADC need about 100us
@@ -79,9 +77,9 @@ void loop() {
               }
             }
             averageP = averageP / average_num;
-            float temp = 16.0; // get Temperature
-            float humidity = 76.0;
-            float diffrent = averageP - (930 + (temp-25.8)*6); // 1100 is ture reference voltage base on my circuits
+            float temp = dht.readTemperature(); // get Temperature
+            float humidity = dht.readHumidity();
+            float diffrent = averageP - (REFVOLT + (temp-25.8)*6); // 1100 is ture reference voltage base on my circuits
             float h_factor = 1.0;
             if(humidity <= 50){
               h_factor = 1;
@@ -96,17 +94,6 @@ void loop() {
             else {
                 historyP = (historyP * 0.95 + pm25ug * 0.05);
             }
-            lcd.clear();
-            lcd.setCursor(0,0);
-            lcd.print("PM25: ");
-            lcd.print(historyP); // averaged
-            lcd.setCursor(14,0);
-            lcd.print("ug");
-            lcd.setCursor(0,1);
-            lcd.print("Volt: ");
-            lcd.print(averageP);
-            lcd.setCursor(14,1);
-            lcd.print("mV");
             // post to Serial
             Serial.print("T: ");
             Serial.print(temp, 2);
@@ -126,3 +113,4 @@ void loop() {
         }
     }
 }
+
